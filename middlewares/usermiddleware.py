6d5c6247d74cade585312466a7999cbac835403f
darklorianprime <lolpokens@mail.ru>
2022-01-31 16:38:50 +0400

    Replace all get_user function, replace serializers

--- middlewares/usermiddleware.py
+++ middlewares/usermiddleware.py
@@ -1,4 +1,4 @@
-from django.db.models import Exists
+from django.db.models import Exists, OuterRef
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework.authtoken.models import Token
 
@@ -8,6 +8,6 @@ from authorizationserver.models import User
 class UserMiddleware(MiddlewareMixin):
     def process_request(self, request):
         token = request.headers.get('authorization')
-        user = User.objects.filter(Exists(Token.objects.filter(key=token.split(' ')[1]))).first()
+        user = User.objects.filter(Exists(Token.objects.filter(key=token.split(' ')[1], user_id=OuterRef('id')))).first()
         request.user_data = user
         return
